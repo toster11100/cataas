f5de492b1badf9933d5dc1361f277ce97298bcfd
toster11100 <victorchuguev@gmail.com>
2022-11-11 00:09:38 +0400

    2.0.3.1 fix log name

--- cataas.go
+++ cataas.go
@@ -22,40 +22,44 @@ type YamlConfig struct {
 }
 
 type Flag struct {
-	Tag, Says, Filter, Config *string
-	Height, Width             *int
+	Tag    *string
+	Says   *string
+	Filter *string
+	Config *string
+	Height *int
+	Width  *int
 }
 
 func main() {
 	if len(os.Args) == 1 {
-		log.Fatal("Please give me one argument!\n")
+		log.Fatal("Please give me one argument!")
 	}
 	arg := Flag{}
 	arg.Tag = pflag.StringP("tag", "t", "", "tag cats")
 	arg.Says = pflag.StringP("says", "s", "", "cat will say hello")
 	arg.Filter = pflag.StringP("filter", "f", "", "filter for cute cats")
-	arg.Height = pflag.IntP("height", "h", 0, "image height")
 	arg.Config = pflag.StringP("config", "c", "./config.yaml", "yaml config")
+	arg.Height = pflag.IntP("height", "h", 0, "image height")
 	arg.Width = pflag.IntP("width", "w", 0, "image width")
 	pflag.Parse()
 
 	filename := pflag.Arg(0)
 	if filename == "" {
-		log.Fatal("no name in arguments\n")
+		log.Fatal("no name in arguments")
 	}
 
-	FileYAML, err := os.ReadFile(*arg.Config)
+	fileYAML, err := os.ReadFile(*arg.Config)
 	if err != nil {
-		log.Fatal("problem with yaml config\n", err)
+		log.Fatalf("problem with yaml confi %v", err)
 	}
 	c := YamlConfig{}
-	yaml.Unmarshal(FileYAML, &c)
+	yaml.Unmarshal(fileYAML, &c)
 
-	URL := c.SayMyURL(arg)
+	URL := c.BuildURL(arg)
 
 	response, err := http.Get(URL.String())
 	if err != nil {
-		log.Fatal("website access problems\n", err)
+		log.Fatalf("website access problems %v", err)
 	}
 	defer response.Body.Close()
 	TypeImage, err := GetFormat(response.Header.Get("Content-Type"))
@@ -65,23 +69,23 @@ func main() {
 
 	if filename == "-" {
 		if _, err := io.Copy(os.Stdout, response.Body); err != nil {
-			log.Fatal("cannot be output to command line\n", err)
+			log.Fatalf("cannot be output to command line %v", err)
 		}
 
 	} else {
 		file, err := os.Create(filename + TypeImage)
 		if err != nil {
-			log.Fatal("unable to create file\n", err)
+			log.Fatalf("unable to create file %v", err)
 		}
 
 		_, err = io.Copy(file, response.Body)
 		if err != nil {
-			log.Fatal("unable to write file\n", err)
+			log.Fatalf("unable to write file %v", err)
 		}
 	}
 }
 
-func (c YamlConfig) SayMyURL(arg Flag) url.URL {
+func (c YamlConfig) BuildURL(arg Flag) *url.URL {
 	if *arg.Tag == "" {
 		*arg.Tag = c.Tag
 	}
@@ -107,15 +111,15 @@ func (c YamlConfig) SayMyURL(arg Flag) url.URL {
 	if *arg.Height != 0 {
 		v.Set("height", strconv.Itoa(*arg.Height))
 	}
-	URL := url.URL{
+	URL := &url.URL{
 		Scheme:   "https",
 		Host:     "cataas.com",
 		Path:     "/cat",
 		RawQuery: v.Encode(),
 	}
-	URL = *URL.JoinPath(*arg.Tag)
+	URL = URL.JoinPath(*arg.Tag)
 	if *arg.Says != "" {
-		URL = *URL.JoinPath("says", *arg.Says)
+		URL = URL.JoinPath("says", *arg.Says)
 	}
 	return URL
 }
